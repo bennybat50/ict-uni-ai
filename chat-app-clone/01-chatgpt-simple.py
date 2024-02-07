fe18c6e62e8a3840e0bf064b4130802e3c802cfd
Young Batimehin <benny.bat51@gmail.com>
2024-02-07 10:24:27 +0100

    upload

---
+++ chat-app-clone/01-chatgpt-simple.py
@@ -0,0 +1,31 @@
+import openai
+
+openai.api_key = "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"
+print(".... Welcome to Your Sassy Assistant.....")
+
+
+
+def getChoice():
+    print("Choose an option \n1) Start Chat\n0) Exit Chat\n")
+    choice=input("Choose a number\n")
+    
+    if(choice=="1"):
+        getInput()
+    elif (choice=="0"):
+          print("Bye bye")
+          exit()
+
+              
+    
+        
+    
+def getInput() :
+    content=input("Type Anything....")
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}])
+    print(completion.choices[0].message.content)
+    getChoice()
+    
+    
+
+getChoice()
